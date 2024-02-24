58ac641890e93f82eb2edd449e97cf69e5ed63cf
LiLSchw4nz <lilschw4nz@gmail.com>
2024-02-24 13:26:10 +0330

    created the folder for KNN and wrote the initial lines of code

---
+++ K-Nearest Neighbors/knn.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def euclidean_distance(p1, p2):
+    return None
+
+class knn():
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
